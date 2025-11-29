ab88e18275e1350e76c3c9f12fdd40c5091677a9
paldeflex <paldeflex@gmail.com>
2025-11-29 19:30:50 +0500

    Добавил вывод 404 у эндпоинта на получение товара, если он в статусе Draft

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ProductStatus;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -12,6 +13,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::query()
+            ->where('status', ProductStatus::Published)
             ->withAvg('productReviews', 'rating')
             ->with('productImages')
             ->paginate(100);
@@ -26,6 +28,10 @@ class ProductController extends Controller
 
     public function show(Product $product): ProductResource
     {
+        if ($product->status === ProductStatus::Draft) {
+            abort(404);
+        }
+
         $product->loadAvg('productReviews', 'rating')
             ->load('productImages');
 
