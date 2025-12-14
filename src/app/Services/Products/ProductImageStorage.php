173634768ec0bfa6539b95995a9f20cae0070e9a
paldeflex <paldeflex@gmail.com>
2025-12-14 21:27:38 +0500

    Реализовал adapter для файлового хранилища

--- src/app/Services/Products/ProductImageStorage.php
+++ src/app/Services/Products/ProductImageStorage.php
@@ -7,14 +7,17 @@ namespace App\Services\Products;
 use App\Models\Product;
 use App\Services\Contracts\Products\ProductImageStorageInterface;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 final class ProductImageStorage implements ProductImageStorageInterface
 {
+    private const string DISK = 'public';
+
     public function store(Product $product, UploadedFile $file): void
     {
         $directory = 'products/'.$product->id;
 
-        $path = $file->store($directory, 'public');
+        $path = Storage::disk(self::DISK)->putFile($directory, $file);
 
         $product->productImages()->create([
             'path' => $path,
